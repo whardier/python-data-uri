ed05b1a93e37ab70ccf96ba2f83b2684c0e65525
Shane R. Spencer <spencersr@brutetech.com>
2014-11-26 21:51:26 -0900

    this does not seem well and right with the world

--- data_uri/encoder.py
+++ data_uri/encoder.py
@@ -2,16 +2,17 @@ import magic
 import base64
 
 def EncodeBase64(buffer, force_mimetype='', charset=''):
+
     if force_mimetype:
         mimetype = force_mimetype
     else:
         mimetype = magic.from_buffer(buffer, mime=True)
 
-    header_parts = ['data:' + mimetype]
+    header_parts = ['data:' + mimetype.decode('ascii')]
 
     if charset:
         header_parts.append('charset=' + charset)
 
     header_parts.append('base64')
 
-    return ';'.join(header_parts) + ',' + base64.b64encode(buffer)
+    return ';'.join(header_parts) + ',' + base64.b64encode(bytes(buffer.encode('ascii'))).decode('ascii')
